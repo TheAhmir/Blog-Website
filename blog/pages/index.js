e258105d07fcc0f836803e7895f4427336fbe052
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2023-07-01 13:51:31 -0400

    mistakes

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,14 +1,8 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router';
-import { PostCard, Categories, PostWidget, Loader } from '../components'
+import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 
 export default function Home( {posts} ) {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <Loader />;
-  }
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -34,7 +28,6 @@ export async function getStaticProps() {
   const posts = (await getPosts()) || []
 
   return {
-    props: { posts },
-    fallback: true
+    props: { posts }
   }
 }
